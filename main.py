2fca87c3c22cb2635a921b6a4cb2d46ae3fb0ef6
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2024-01-13 19:18:19 +0530

    email titles and description to users

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "50f488bc91474505a8388f4ac83371e9"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-12-13&sortBy=publishedAt&apiKey=50f488bc91474505a8388f4ac83371e9"
@@ -10,6 +11,9 @@ request = requests.get(url)
 content = request.json()
 
 #Access the article titles and description
+body  = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
